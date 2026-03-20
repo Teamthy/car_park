3d1fac04719ae30d6882d881faf66795a7cbe963
Teamthy <olusanyatimothy54@gmail.com>
2026-03-20 06:01:30 +0100

    fix(backend): resolve app export error and connect mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const router = require("./routes/parkingRoutes");
 const app = express();
 
 app.use(express.json());
+
+
 app.use("/api", require("./routes/parkingRoutes"));
+
+
 app.use(require("./middleware/errorMiddleware"));
 
-module.exports = router;
+module.exports = app; 
